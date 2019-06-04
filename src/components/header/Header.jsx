77094685ae789f7f32d42bb8b2f96fee98fb6949
anna-salata <anna.m.salata@gmail.com>
2019-06-04 15:45:05 +0300

    Menu button changed

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {Option} from "./Option";
 import './Header.scss';
 import logo from '../../assets/images/white-camera-logo-png-1.png'
+import menu from '../../assets/images/button.png';
 
 export class Header extends React.Component {
     state = {
@@ -21,7 +22,8 @@ export class Header extends React.Component {
         }
         return <nav className={className}>
             <div className="global-nav__controls controls">
-                <div className="controls__menu" onClick={this.toggle}>Menu</div>
+                <div className="controls__menu" onClick={this.toggle}><img className="controls__menu" src={menu}
+                                                                           alt="меню"/></div>
                 <div className="controls__logo"><img className="controls__logo" src={logo} alt=""/></div>
             </div>
             <ul className="global-nav__options">
